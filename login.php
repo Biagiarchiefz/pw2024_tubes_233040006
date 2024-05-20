fb1979c1e74ffed0e85d3cd4dea8e4bb3ad6f881
Biagiarchiefz <biagiiarchie@gmail.com>
2024-05-20 14:42:54 +0700

    p3

--- login.php
+++ login.php
@@ -14,56 +14,57 @@
 </head>
 
 <body>
- 
+
   <div class="container">
-    <div class="row d-flex justify-content-center align-items-center min-vh-100">
+    <div class="row d-flex justify-content-around align-items-center min-vh-100">
 
       <div class="d1 col-md-6">
-      <div class="p1 mb-5 text-light">
-        <h2>Welcome back!</h2>
-        <p>Enter your details to continue enjoying your favorite tunes.</p>
-    </div>
-    <div class="p2">
-      <div class="mb-2">
-        <label for="exampleFormControlInput1" class="form-label text-light">Username</label>
-        <input type="email" class="form-control rounded-pill" id="exampleFormControlInput1" placeholder="Username">
+        <div class="p1 mb-5 text-light">
+          <h2>Welcome back!</h2>
+          <p>Enter your details to continue enjoying your favorite tunes.</p>
+        </div>
+        <div class="p2">
+          <div class="mb-2">
+            <label for="exampleFormControlInput1" class="form-label text-light">Username</label>
+            <div class="field-group">
+              <input type="email" class="form-control rounded-pill" id="exampleFormControlInput1" placeholder="Username">
+              <div class="icon">
+                <img src="icons/crown.png" alt="">
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-2">
+            <label for="exampleFormControlInput1" class="form-label text-light ">Password</label>
+
+            <div class="field-group">
+              <input type="email" class="form-control rounded-pill" id="exampleFormControlInput1" placeholder="Password">
+              <div class="icon">
+                <img src="icons/lock.png" alt="">
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="p3">
+          <div class="">
+            <img src="" alt="">
+            <button type="button" class="btn btn-primary mb-2 w-100 rounded-pill">Sign in</button>
+          </div>
+          <div class="">
+            <button type="button" class="btn btn-light w-100 rounded-pill">Create New Account</button>
+          </div>
+        </div>
       </div>
 
-      <div class="mb-5">
-        <label for="exampleFormControlInput1" class="form-label text-light ">Password</label>
-        <input type="email" class="form-control rounded-pill" id="exampleFormControlInput1" placeholder="Password">
+      <div class="d2 col-md-6">
 
-      </div>
+        <p class="text-light fw-bold">tune track.</p>
 
-    </div>
-
-    <div class="p3">
-      <div class="">
-        <img src="" alt="">
-        <button type="button" class="btn btn-primary mb-2 w-100 rounded-pill">Sign in</button>
-      </div>
-      <div class="">
-        <button type="button" class="btn btn-light w-100 rounded-pill">Create New Account</button>
       </div>
     </div>
   </div>
-
-  <div class="d2 col-md-6">
-    <div class="row ">
-      <p class="text-light">tune track.</p>
-    </div>
-  </div>
-</div>
-</div>
-
-  
-  
-  
-
-
-
-
-
 
 
 
